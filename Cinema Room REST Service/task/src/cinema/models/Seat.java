409d8eccc6633fb6f2107ada5158edd384b69475
sergio-villanueva <94728439+sergio-villanueva@users.noreply.github.com>
2022-03-12 20:37:49 -0500

    Solved Request Error
    purchaseTicket and refundTicket request errors solved

--- Cinema Room REST Service/task/src/cinema/models/Seat.java
+++ Cinema Room REST Service/task/src/cinema/models/Seat.java
@@ -4,6 +4,8 @@ public class Seat {
     private int row;
     private int column;
     private int price;
+    
+    public Seat() {}
 
     public Seat(int row, int column) {
         this.row = row;
